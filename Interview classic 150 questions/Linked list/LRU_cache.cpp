b88a3b78bb0b881aa462c82e77a8e609948308f9
smr <2152918099@qq.com>
2024-07-30 22:12:00 +0800

    sixteenth day

--- Interview classic 150 questions/Linked list/LRU_cache.cpp
+++ Interview classic 150 questions/Linked list/LRU_cache.cpp
@@ -1,18 +1,72 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
+struct DLinkedList {
+    int key, val;
+    DLinkedList *pre;
+    DLinkedList *next;
+
+    DLinkedList(): key(0), val(0), pre(nullptr), next(nullptr) {
+    }
+
+    DLinkedList(int _key, int _val): key(_key), val(_val), pre(nullptr), next(nullptr) {
+    }
+};
+
 class LRUCache {
+private:
+    DLinkedList *dummy;
+    unordered_map<int, DLinkedList *> map;
+    int capacity;
+
+    void deleteNode(DLinkedList *node) {
+        node->pre->next = node->next;
+        node->next->pre = node->pre;
+    }
+
+    void moveToHead(DLinkedList *node) {
+        deleteNode(node);
+        node->pre = dummy;
+        node->next = dummy->next;
+        dummy->next = node;
+        node->next->pre = node;
+    }
+
 public:
     LRUCache(int capacity) {
-
+        this->capacity = capacity;
+        dummy = new DLinkedList();
+        dummy->pre = dummy;
+        dummy->next = dummy;
     }
 
     int get(int key) {
-
+        if (map.count(key)) {
+            moveToHead(map[key]);
+            return map[key]->val;
+        }
+        return -1;
     }
 
     void put(int key, int value) {
-
+        if (map.count(key)) {
+            map[key]->val = value;
+            moveToHead(map[key]);
+        } else {
+            auto *p = new DLinkedList(key, value);
+            p->pre = dummy;
+            p->next = dummy->next;
+            dummy->next = p;
+            p->next->pre = p;
+            map.insert({key, p});
+            if (map.size() > capacity) {
+                p = dummy->pre;
+                deleteNode(p);
+                map.erase(p->key);
+                delete p;
+            }
+        }
     }
 };
 
