883798fb7735043c72e8089a5bb7e703213bffe0
smr <2152918099@qq.com>
2024-07-31 21:12:38 +0800

    seventeenth day

---
+++ Interview classic 150 questions/binary tree/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Solution {
+public:
+    TreeNode *build(vector<int>&preorder,vector<int>&inorder) {
+
+    }
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+
+    }
+};
