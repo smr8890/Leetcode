cc9a3dad551fa42d0829ff8d9b684e0177d3113d
smr <2152918099@qq.com>
2024-08-14 21:43:01 +0800

    twenty-eight day

---
+++ Interview classic 150 questions/Kadane algorithm/Maximum_Subarray.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int> &nums) {
+        if (nums.size() == 1)return nums[0];
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
